409c46d6be574776600cde0bf8155f417dba452e
Haru Onodera <h.onodera.sys21@morijyobi.ac.jp>
2022-06-30 14:36:05 +0900

    group8更新

--- group8/src/servlet/LoginServlet.java
+++ group8/src/servlet/LoginServlet.java
@@ -2,7 +2,6 @@ package servlet;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,8 +14,7 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/u_login.jsp");
-		dispatcher.forward(request, response);
+		
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
