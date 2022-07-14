91d3ed1a789c24866d8e32bcc253a2f5326e7698
Haru Tamura <haru.tamura.sys21@morijyobi.ac.jp>
2022-07-14 10:46:29 +0900

    tamura

--- group8/src/DAO/LoginDAO.java
+++ group8/src/DAO/LoginDAO.java
@@ -1,31 +1,41 @@
 package DAO;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import scopedata.Account;
+
 public class LoginDAO {
-	public int execute(Account account) {
-		int f = 0;
+	public Account find(String _wid,String _pass) {
+		Account account = null;
 		Connection con = null;
 		try {
 			Class.forName("org.h2.Driver");
-			con = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/group8","sa","pass");
-			String sql = "insert into administer values(?,?)";
+			con=DriverManager.getConnection("jdbc:h2:tcp://localhost/~/group8","sa","pass");
+			String sql = "select * from group8";
 			PreparedStatement pstmt = con.prepareStatement(sql);
-			pstmt.setString(1,account.getId());
-			pstmt.setString(2,account.getPass());
-			f=pstmt.executeUpdate();
+			pstmt.setString(1, _wid);
+			pstmt.setString(2, _pass);
+			ResultSet rs = pstmt.executeQuery();
+			rs.next();
+			String id = rs.getString("id");
+			String pass = rs.getString("pass");
+			String name = rs.getString("name");
+			account = new Account(id,pass,name);
 		}catch(Exception e) {
-			System.out.println(e.getMessage());
+			System.out.print("exception error!");
+			return null;
 		}finally {
 			try {
-				if(con!=null)	con.close();
+				if(con!=null) con.close();
 			}catch(SQLException e) {
-				e.printStackTrace();
+				System.out.print("SQL error!");
+				return null;
 			}
 		}
-		return f;
+		return account;
 	}
 }
--- group8/src/servlet/A_LoginServlet.java
+++ group8/src/servlet/A_LoginServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import DAO.LoginDAO;
+import scopedata.Account;
 @WebServlet("/A_LoginServlet")
 public class A_LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -15,7 +19,22 @@ public class A_LoginServlet extends HttpServlet {
 		dispatcher.forward(request, response);
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+		String pass = request.getParameter("pass");
+
+		LoginDAO f = new LoginDAO();
+		Account account =f.find(id,pass);
+		HttpSession session = request.getSession();
+		session.setAttribute("Login", account);
+		String path= null;
+		if(account!= null) { //データあり
+			path="WEB-INF/jsp/a_co_list.jsp";
+		}else {             //データなし
+			path="WEB-INF/jsp/a_login.jsp";
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+		dispatcher.forward(request, response);
 	}
 }
 
