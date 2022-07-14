ac49aaf1166317a128328ec8168549d16fce563d
Haru Onodera <h.onodera.sys21@morijyobi.ac.jp>
2022-07-14 10:05:57 +0900

    晴

---
+++ group8/src/DAO/A_registerDAO.java
@@ -0,0 +1,43 @@
+package DAO;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import scopedata.Account;
+public class A_registerDAO {
+	public int execute(Account account) {
+		int f = 0;
+		Connection con = null;
+		try {
+			Class.forName("org.h2.Driver");
+			con = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/group8","sa","pass");
+			String sql = "insert into administer values(?,?)";
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			pstmt.setString(1,account.getId());
+			pstmt.setString(2,account.getPass());
+			f=pstmt.executeUpdate();
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}finally {
+			try {
+				if(con!=null)	con.close();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return f;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
