8d386b903e67341f4434883a59919467e8cb3d3a
SYS-4211106 <s.umekawa.sys21@morijyobi.ac.jp>
2022-07-14 10:53:25 +0900

    umekawa

--- group8/src/servlet/SearchServlet.java
+++ group8/src/servlet/SearchServlet.java
@@ -1,18 +1,38 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import scopedata.Company;
 
 @WebServlet("/SearchServlet")
 public class SearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String action=request.getParameter("action");
+		String path=null;
+		
+		HttpSession session=request.getSession();
+		if(action.equals("menu_jyouken")) {
+			List<Company> list=new ArrayList<Company>();
+			session.setAttribute("list", list);
+			path="WEB-INF/jsp/companySearch.jsp";
+			
+			
+		RequestDispatcher dispatcher =
+				request.getRequestDispatcher(path);
+		dispatcher.forward(request, response);
 
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
