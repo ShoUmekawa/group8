b9ad24b4a9c83bc7cdd9ad8174f97338f50ddc18
Haru Onodera <h.onodera.sys21@morijyobi.ac.jp>
2022-07-15 10:05:15 +0900

    晴

--- group8/src/servlet/LoginServlet.java
+++ group8/src/servlet/LoginServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import DAO.LoginDAO;
+import scopedata.Account;
 
 
 @WebServlet("/LoginServlet")
@@ -19,7 +23,23 @@ public class LoginServlet extends HttpServlet {
 		dispatcher.forward(request, response);
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/u_menu.jsp");
+		String id  = request.getParameter("id");
+		String pass = request.getParameter("pass");
+
+		LoginDAO f = new LoginDAO();
+		Account  account = f.find(id , pass);
+
+		HttpSession session = request.getSession();
+		session.setAttribute("Login",account);
+
+		String path = null;
+
+		if(account != null) {
+			path = "WEB-INF/jsp/u_menu.jsp";
+		}else {
+			path = "WEB-INF/jsp/u_login_err.jsp";
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
 		dispatcher.forward(request, response);
 	}
 
